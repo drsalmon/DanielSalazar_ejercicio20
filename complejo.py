d089637e09f12e65a59da61effeabfa82556cb63
Default user <danielrisalazar@hotmail.com>
2019-03-28 12:52:32 +0000

    lo que sea

---
+++ complejo.py
@@ -0,0 +1,16 @@
+import math
+import numpy as np
+
+class Complejo():
+    def __init__(self, imaginario, real):
+        self.imaginario = imaginario
+        self.real = real
+        self.norma = math.sqrt((self.real**2) + (self.imaginario**2))
+    def conjugado(self):
+        c = self.real + self.imaginario*(j)
+        return c.conjugate()
+    def calcula_norma(self):
+        return norma
+    def pow():
+        return math.pow()
+        
